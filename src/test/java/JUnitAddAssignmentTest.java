6b6e9fa1e14f7c9444bd8aca8bce49a127406450
VladDragoi <vdrag01@safeway.com>
2020-05-06 00:32:02 +0300

    Implemented riguros test cases for AddAssignment feature

--- src/test/java/JUnitAddAssignmentTest.java
+++ src/test/java/JUnitAddAssignmentTest.java
@@ -1,8 +1,17 @@
+import domain.Nota;
+import domain.Student;
 import domain.Tema;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import repository.NotaXMLRepository;
+import repository.StudentXMLRepository;
 import repository.TemaXMLRepository;
+import service.Service;
+import validation.NotaValidator;
+import validation.StudentValidator;
 import validation.TemaValidator;
+import validation.Validator;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -11,12 +20,58 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 public class JUnitAddAssignmentTest {
 
     TemaXMLRepository temaXMLRepository;
-    TemaValidator temaValidator;
+    Service service;
 
     @Before
     public void setup() {
-        temaValidator = new TemaValidator();
+        Validator<Student> studentValidator = new StudentValidator();
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
+
+        StudentXMLRepository studentXMLRepository = new StudentXMLRepository(studentValidator, "studenti.xml");
         temaXMLRepository = new TemaXMLRepository(temaValidator, "teme.xml");
+        NotaXMLRepository notaXMLRepository = new NotaXMLRepository(notaValidator, "note.xml");
+
+        service = new Service(studentXMLRepository, temaXMLRepository, notaXMLRepository);
+    }
+
+    @Test
+    public void addAssignmentWithMissingId()
+    {
+        Tema assignment = new Tema("", "desc", 5, 3);
+        int resultSave = service.saveTema(assignment.getID(), assignment.getDescriere(), assignment.getDeadline(), assignment.getStartline());
+        Assert.assertEquals(resultSave, 1);
+    }
+
+    @Test
+    public void addAssignmentWithMissingDescription()
+    {
+        Tema assignment = new Tema("tema", "", 10, 3);
+        int resultSave = service.saveTema(assignment.getID(), assignment.getDescriere(), assignment.getDeadline(), assignment.getStartline());
+        Assert.assertEquals(resultSave, 1);
+    }
+
+    @Test
+    public void addAssignmentWithWrongDeadline()
+    {
+        Tema assignment = new Tema("tema", "description", 2020, 2);
+        int resultSave = service.saveTema(assignment.getID(), assignment.getDescriere(), assignment.getDeadline(), assignment.getStartline());
+        Assert.assertEquals(resultSave, 1);
+    }
+
+    @Test
+    public void addAssignmentWithWrongStartline()
+    {
+        Tema assignment = new Tema("tema2", "description", 4, -5);
+        int resultSave = service.saveTema(assignment.getID(), assignment.getDescriere(), assignment.getDeadline(), assignment.getStartline());
+        Assert.assertEquals(resultSave, 1);
+    }
+
+    @Test
+    public void addCorrectAssignmentTest() {
+        Tema assignment = new Tema("tema", "description", 4, 1);
+        int resultSave = service.saveTema(assignment.getID(), assignment.getDescriere(), assignment.getDeadline(), assignment.getStartline());
+        Assert.assertEquals(resultSave, 0);
     }
 
     @Test
@@ -33,7 +88,7 @@ public class JUnitAddAssignmentTest {
 
     @Test
     public void addAssignmentExceptionTest() {
-        Tema tema = new Tema("", "Tema veche", 12, 10);
+        Tema tema = new Tema("", "", 500, -10);
         Tema temaNoua = temaXMLRepository.save(tema);
 
         assertNull(temaNoua);
